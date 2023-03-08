9f689cf01ce307e0d152bd96dc2919d9ef8c94d0
Zaid alshibi <zaidealshibi@gmail.com>
2023-03-08 23:13:05 +0300

    added ssl to the database connection

--- DataBase/index.js
+++ DataBase/index.js
@@ -7,12 +7,12 @@ const records = require( './models.js' );
 const DATABASE_URL = process.env.DATABASE_URL || 'postgres://zaid:1470@localhost:5432/postgres';
 
 const sequelizeOption = {
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true,
-    //         rejectUnauthorized: false
-    //     }
-    // }
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 };
 
 const sequelize = new Sequelize( DATABASE_URL, sequelizeOption );
